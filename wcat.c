a6914ddfebdd0819933c03678ab5a329ed450354
Katherine ;) <kcjesse02@gmail.com>
2023-01-16 14:00:44 -0500

    changes to wcat

--- wcat.c
+++ wcat.c
@@ -1,3 +1,9 @@
 int main(int argc, char *argv[]){
-    
+    int a;
+    char *str[60]
+    for(a = 1; a < argc; a++){
+        int f = open(argv[a]);
+        assert(f>-1);
+        printf("%s", fgets());
+    }
 }
