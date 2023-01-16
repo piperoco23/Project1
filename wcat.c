45f9d8774f72ff2aeeb5e8361ae51ad919c96960
Katherine ;) <kcjesse02@gmail.com>
2023-01-16 14:38:03 -0500

    wcat for you

--- wcat.c
+++ wcat.c
@@ -1,9 +1,15 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[]){
     int a;
-    char *str[60]
-    for(a = 1; a < argc; a++){
-        int f = open(argv[a]);
-        assert(f>-1);
-        printf("%s", fgets());
+    char *str[60];
+    FILE *f;
+    for(a = 1; a < argc; a=a+1){
+        f = fopen(argv[a], "r");
+        if(f != NULL){
+            if(fgets(str, 60, f) != NULL){
+                puts(fgets(str, 60, f));
+            }
+        }
     }
 }
