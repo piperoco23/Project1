5ffc43bc5eed40136f0aab861ae4f5bb518a9d6c
Katherine ;) <kcjesse02@gmail.com>
2023-01-17 11:44:21 -0500

    only small errors

--- project-utilities/wcat.c
+++ project-utilities/wcat.c
@@ -2,21 +2,21 @@
 
 int main(int argc, char *argv[]){
     int a;
-    char *str[60];
+    char str[60];
     FILE *f;
     for(a = 1; a < argc; a=a+1){
         f = fopen(argv[a], "r");
         if(f != NULL){
             if(fgets(str, 60, f) != NULL){
-                puts(fgets(str, 60, f));
+                puts(str);
             }
             else{
-                exit(1);
+                return 1;
             }
         }
         else{
             printf("wcat: cannot open file\n");
-            exit(1);
+            return 1;
         }
     }
     return 0;
