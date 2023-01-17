1167d307dbe73e55e4ccab00336cadd75eeaf1c8
Katherine Jesse <kcjesse02@gmail.com>
2023-01-17 13:16:04 -0500

    WCAT WORKS

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 Names: Piper O'Connell, Katherine Jesse
 
 Here is our checkpoint submission.
-we have completed part 1 and 2.
-wcat is partially complete and will not compile. Please do not try to compile it.
+we have completed part 1, 2, and 3.
--- project-utilities/wcat.c
+++ project-utilities/wcat.c
@@ -6,7 +6,9 @@ int main(int argc, char *argv[]){
     char c;
     for(a = 1; a < argc; a=a+1){
         f = fopen(argv[a], "r");
+        //this if checks if f was able to be retrieved
         if(f != NULL){
+                //iterates through each character and pprints each out individually
                 while(1) {
                     c = fgetc(f);
                     if( feof(f) ) { 
@@ -16,6 +18,7 @@ int main(int argc, char *argv[]){
                 }
             }
         else{
+            //returns with error code 1 if file cannot be opened
             printf("wcat: cannot open file\n");
             return 1;
         }
