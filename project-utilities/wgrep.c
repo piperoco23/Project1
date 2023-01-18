d916356e0ab768f2189f4e140d658f5adce679d5
Katherine Jesse <kcjesse02@gmail.com>
2023-01-18 14:05:19 -0500

    wgep skeleton

--- project-utilities/wgrep.c
+++ project-utilities/wgrep.c
@@ -1,1 +1,15 @@
-Place code here.
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int a;
+    FILE *f;
+    char c;
+    char *search = argv[1];
+    for(a = 2; a<argc; a=a+1){
+        f = fopen(argv[a], "r");
+        if( f == NULL){
+            printf("wgrep: cannot open file\n");
+            return 1;
+        }
+    }
+}
