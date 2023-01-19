03b46b08f216032c104c11540ce6b3f7b5a8c052
Katherine Jesse <kcjesse02@gmail.com>
2023-01-19 11:25:20 -0500

    comments

--- project-utilities/wcat.c
+++ project-utilities/wcat.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-    int a;
-    FILE *f;
-    char c;
+    int a; //for loop variable
+    FILE *f; //current file will be stored here
+    char c; //current character will be stored here
+
+    //for loop iterates through each argument, which should represent a file path
     for(a = 1; a < argc; a=a+1){
         f = fopen(argv[a], "r");
-        //this if checks if f was able to be retrieved
+        //this if checks if f was able to be opened (a valid file path was give)
         if(f != NULL){
-                //iterates through each character and pprints each out individually
+                //iterates through each character and prints each out individually
                 while(1) {
                     c = fgetc(f);
+                    //ends the loop when end-of-file is reached
                     if( feof(f) ) { 
                         break ;
                     }
--- project-utilities/wgrep.c
+++ project-utilities/wgrep.c
@@ -3,11 +3,9 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    int a;
-    FILE *f;
-    //char c;
-    //string we want to search the file for
-    char *search = argv[1];
+    int a; //for loop variable
+    FILE *f; //file we will search in
+    char *search = argv[1]; //string we want to search the file for
 
     //size of the buffer for the input line
     size_t buffer = 500;
@@ -23,34 +21,38 @@ int main(int argc, char *argv[]){
     else if(argc == 2){
         while(1){
             getline(&line, &buffer, stdin);
+            //stops reading file when end-of-file is reached
             if(feof(stdin)){
                     break;
                 }
+            //prints out the current line if search term is contained within the line
             if(strstr(line, search) != NULL){
                 printf("%s", line);
             }
-            //free(line);
         }
         return 0;
     }
-
+    //cases where 1 or more files are given
+    //for loop iterates through each argument starting at the second (When the first file path should be given)
     for(a = 2; a<argc; a=a+1){
         f = fopen(argv[a], "r");
+        //error case where file cannot be opened (the file path given wasnt valid)
         if( f == NULL){
             puts("wgrep: cannot open file");
             return 1;
         }
         else{
-
+            //iterates through each line
             while(1){
                 getline(&line, &buffer, f);
+                //stops reading file when end-of-file is reached
                 if(feof(f)){
                     break;
                 }
+                //prints out the current line if search term is contained within the line
                 if(strstr(line, search) != NULL){
                     printf("%s", line);
                 }
-                //free(line);
             }
 
         }
